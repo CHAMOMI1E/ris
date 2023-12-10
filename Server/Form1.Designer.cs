f3c29932c67a156b9e72bfb4fe969de1e36f52cf
ILYA <118203026+CHAMOMI1E@users.noreply.github.com>
2023-12-10 22:54:51 +0300

    add lab7

---
+++ Server/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Lab7
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            startButton = new Button();
+            SuspendLayout();
+            // 
+            // startButton
+            // 
+            startButton.Location = new Point(95, 58);
+            startButton.Name = "startButton";
+            startButton.Size = new Size(137, 52);
+            startButton.TabIndex = 0;
+            startButton.Text = "Запуск сервера";
+            startButton.UseVisualStyleBackColor = true;
+            startButton.Click += startButton_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(360, 177);
+            Controls.Add(startButton);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button startButton;
+    }
+}
